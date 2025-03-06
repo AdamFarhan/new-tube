62a50cac9c950de4d0459b0cb6d7232fdf1f2a75
Adam Farhan <adamfarhan11@gmail.com>
2025-03-05 22:44:11 -0500

    Added basic nav wrapper UI

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-export default function Home() {
-  return (
-    <div>
-      <p className="font-bold text-rose-500">Hello World</p>
-      <Button>Click Me</Button>
-    </div>
-  );
-}
